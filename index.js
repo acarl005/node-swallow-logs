be0a35107feb39c8e781ae503b894d3424b76400
Andrew Carlson <acarl005@g.ucla.edu>
2016-07-13 16:42:58 -0700

    add console.error

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var swallowLogs = module.exports = function swallowLogs(proc) {
   var consoleLog = console.log
+  var consoleError = console.error
   console.log = () => void 0
+  console.error = () => void 0
   proc()
   console.log = consoleLog
+  console.error = consoleError
 }
 
